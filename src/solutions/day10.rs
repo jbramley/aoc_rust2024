11a6be35c942d6d167bdf46b9708e01d4d12a270
Justin Bramley <jbramley@gmail.com>
2024-12-11 06:02:17 -0500

    Day 10 and Day 11

--- src/solutions/day10.rs
+++ src/solutions/day10.rs
@@ -1,10 +1,95 @@
+use std::collections::{HashSet, VecDeque};
 use std::fs::File;
 use std::io::{BufReader, Lines};
 
-pub fn part1(_lines: Lines<BufReader<File>>) -> String {
-    "Not implemented yet".to_string()
+pub fn part1(lines: Lines<BufReader<File>>) -> String {
+    let topomap = lines
+        .flatten()
+        .map(|s| {
+            s.chars()
+                .map(|c| c.to_digit(10).unwrap())
+                .collect::<Vec<_>>()
+        })
+        .collect::<Vec<_>>();
+    let max_y = topomap.len() - 1;
+    let max_x = topomap[0].len() - 1;
+    let mut total = 0;
+    for (row, line) in topomap.iter().enumerate() {
+        line.iter()
+            .enumerate()
+            .filter(|(_, c)| **c == 0)
+            .for_each(|(col, c)| {
+                let mut trails: VecDeque<(usize, usize, &u32)> = VecDeque::new();
+                let mut trailends: HashSet<(usize, usize)> = HashSet::new();
+                trails.push_back((row, col, c));
+                while !trails.is_empty() {
+                    if let Some((y, x, c)) = trails.pop_front() {
+                        if *c == 9 {
+                            trailends.insert((x, y));
+                            continue;
+                        }
+                        if y > 0 && topomap[y - 1][x] == *c + 1 {
+                            trails.push_back((y - 1, x, &topomap[y - 1][x]));
+                        }
+                        if y < max_y && topomap[y + 1][x] == *c + 1 {
+                            trails.push_back((y + 1, x, &topomap[y + 1][x]));
+                        }
+                        if x > 0 && topomap[y][x - 1] == *c + 1 {
+                            trails.push_back((y, x - 1, &topomap[y][x - 1]));
+                        }
+                        if x < max_x && topomap[y][x + 1] == *c + 1 {
+                            trails.push_back((y, x + 1, &topomap[y][x + 1]));
+                        }
+                    }
+                }
+                total += trailends.len();
+            })
+    }
+
+    format!("{}", total)
 }
 
-pub fn part2(_lines: Lines<BufReader<File>>) -> String {
-    "Not implemented yet".to_string()
+pub fn part2(lines: Lines<BufReader<File>>) -> String {
+    let topomap = lines
+        .flatten()
+        .map(|s| {
+            s.chars()
+                .map(|c| c.to_digit(10).unwrap())
+                .collect::<Vec<_>>()
+        })
+        .collect::<Vec<_>>();
+    let max_y = topomap.len() - 1;
+    let max_x = topomap[0].len() - 1;
+    let mut total = 0;
+    for (row, line) in topomap.iter().enumerate() {
+        line.iter()
+            .enumerate()
+            .filter(|(_, c)| **c == 0)
+            .for_each(|(col, c)| {
+                let mut trails: VecDeque<(usize, usize, &u32)> = VecDeque::new();
+                trails.push_back((row, col, c));
+                while !trails.is_empty() {
+                    if let Some((y, x, c)) = trails.pop_front() {
+                        if *c == 9 {
+                            total += 1;
+                            continue;
+                        }
+                        if y > 0 && topomap[y - 1][x] == *c + 1 {
+                            trails.push_back((y - 1, x, &topomap[y - 1][x]));
+                        }
+                        if y < max_y && topomap[y + 1][x] == *c + 1 {
+                            trails.push_back((y + 1, x, &topomap[y + 1][x]));
+                        }
+                        if x > 0 && topomap[y][x - 1] == *c + 1 {
+                            trails.push_back((y, x - 1, &topomap[y][x - 1]));
+                        }
+                        if x < max_x && topomap[y][x + 1] == *c + 1 {
+                            trails.push_back((y, x + 1, &topomap[y][x + 1]));
+                        }
+                    }
+                }
+            })
+    }
+
+    format!("{}", total)
 }
--- src/solutions/day11.rs
+++ src/solutions/day11.rs
@@ -1,10 +1,87 @@
+use std::collections::HashMap;
 use std::fs::File;
 use std::io::{BufReader, Lines};
 
-pub fn part1(_lines: Lines<BufReader<File>>) -> String {
-    "Not implemented yet".to_string()
+pub fn part1(lines: Lines<BufReader<File>>) -> String {
+    let mut stones = lines
+        .flatten()
+        .next()
+        .unwrap()
+        .split_ascii_whitespace()
+        .map(|s| s.to_string())
+        .collect::<Vec<String>>();
+    for _ in 0..25 {
+        let mut new_stones: Vec<String> = Vec::new();
+        stones.iter().for_each(|s| {
+            if s == "0" {
+                new_stones.push("1".to_string());
+            } else if s.len() % 2 == 0 {
+                let s = s.split_at(s.len() / 2);
+                [s.0, s.1]
+                    .iter()
+                    .map(|sp| format!("{}", sp.parse::<u64>().unwrap()))
+                    .for_each(|sx| new_stones.push(sx));
+            } else {
+                new_stones.push(format!("{}", s.parse::<u64>().unwrap() * 2024u64));
+            }
+        });
+        stones = new_stones.clone();
+        // println!("{}: {:?}", stones.len(), stones);
+    }
+    format!("{}", stones.len())
 }
 
-pub fn part2(_lines: Lines<BufReader<File>>) -> String {
-    "Not implemented yet".to_string()
+pub fn count_stones_after_blinks(
+    s: &str,
+    blinks: u8,
+    memo: &mut HashMap<(String, u8), u64>,
+) -> u64 {
+    if memo.contains_key(&(s.to_string(), blinks)) {
+        memo[&(s.to_string(), blinks)]
+    } else if blinks == 0 {
+        1
+    } else if s == "0" {
+        let ct = count_stones_after_blinks("1", blinks - 1, memo);
+        memo.insert((s.to_string(), blinks), ct);
+        ct
+    } else if s.len() % 2 == 0 {
+        let st = s.split_at(s.len() / 2);
+        let ct = [st.0, st.1]
+            .iter()
+            .map(|sp| {
+                count_stones_after_blinks(
+                    format!("{}", sp.parse::<u64>().unwrap()).as_str(),
+                    blinks - 1,
+                    memo,
+                )
+            })
+            .sum();
+        memo.insert((s.to_string(), blinks), ct);
+        ct
+    } else {
+        let ct = count_stones_after_blinks(
+            format!("{}", s.parse::<u64>().unwrap() * 2024u64).as_str(),
+            blinks - 1,
+            memo,
+        );
+        memo.insert((s.to_string(), blinks), ct);
+        ct
+    }
+}
+
+pub fn part2(lines: Lines<BufReader<File>>) -> String {
+    let mut stones = lines
+        .flatten()
+        .next()
+        .unwrap()
+        .split_ascii_whitespace()
+        .map(|s| s.to_string())
+        .collect::<Vec<String>>();
+
+    let mut memo: HashMap<(String, u8), u64> = HashMap::new();
+    let mut total_stones = 0u64;
+    for s in stones {
+        total_stones += count_stones_after_blinks(s.as_str(), 75, &mut memo);
+    }
+    format!("{}", total_stones)
 }
