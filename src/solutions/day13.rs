84bde9d642d648891aad510eb8c0777152d8292b
Justin Bramley <jbramley@gmail.com>
2024-12-14 08:15:51 -0500

    Day 13 - I definitely took the least bait in the puzzle and overthought the solution for part 2, going down a MILP rabbit hole. Day 14 was fun.

--- src/solutions/day13.rs
+++ src/solutions/day13.rs
@@ -1,10 +1,97 @@
+use itertools::Itertools;
+use num::integer::gcd;
+use std::cmp::min;
 use std::fs::File;
 use std::io::{BufReader, Lines};
 
-pub fn part1(_lines: Lines<BufReader<File>>) -> String {
-    "Not implemented yet".to_string()
+fn parse_button(button_str: &str) -> (u64, u64) {
+    let v = button_str
+        .split_ascii_whitespace()
+        .skip(2)
+        .map(|s| {
+            let (_, s) = s.split_at(2);
+            s.trim_end_matches(",").parse::<u64>().unwrap()
+        })
+        .collect::<Vec<_>>();
+    (v[0], v[1])
 }
 
-pub fn part2(_lines: Lines<BufReader<File>>) -> String {
-    "Not implemented yet".to_string()
+fn parse_prize(prize_str: &str) -> (u64, u64) {
+    let v = prize_str
+        .split_ascii_whitespace()
+        .skip(1)
+        .map(|s| {
+            let (_, s) = s.split_at(2);
+            s.trim_end_matches(",").parse::<u64>().unwrap()
+        })
+        .collect::<Vec<_>>();
+    (v[0], v[1])
+}
+
+fn parse_prize_p2(prize_str: &str) -> (u64, u64) {
+    let v = prize_str
+        .split_ascii_whitespace()
+        .skip(1)
+        .map(|s| {
+            let (_, s) = s.split_at(2);
+            s.trim_end_matches(",").parse::<u64>().unwrap()
+        })
+        .collect::<Vec<_>>();
+    (v[0] + 10000000000000, v[1] + 10_000_000_000_000)
+}
+pub fn part1(lines: Lines<BufReader<File>>) -> String {
+    const TOKENS_A: u64 = 3;
+    const TOKENS_B: u64 = 1;
+
+    let mut min_tokens = 0u64;
+    for mut arcade in &lines.flatten().chunks(4) {
+        let a = parse_button(arcade.next().unwrap().as_str());
+        let b = parse_button(arcade.next().unwrap().as_str());
+        let prize = parse_prize(arcade.next().unwrap().as_str());
+        let max_a = min(100, min(prize.0 / a.0, prize.1 / a.1));
+        let mut tokens = 401;
+        for i in 0..=max_a {
+            let x = prize.0 - i * a.0;
+            let y = prize.1 - i * a.1;
+            if x % b.0 == 0 && y % b.1 == 0 && x / b.0 == y / b.1 && x / b.0 <= 100 {
+                let t = i * TOKENS_A + (x / b.0) * TOKENS_B;
+                if t < tokens {
+                    tokens = t;
+                }
+            }
+        }
+        if tokens < 400 {
+            println!(
+                "Prize ({},{}) gotten in {} tokens",
+                prize.0, prize.1, tokens
+            );
+            min_tokens += tokens;
+        } else {
+            println!("Prize ({},{}) can't be won", prize.0, prize.1);
+        }
+    }
+    format!("{}", min_tokens)
+}
+
+pub fn part2(lines: Lines<BufReader<File>>) -> String {
+    const TOKENS_A: u64 = 3;
+    const TOKENS_B: u64 = 1;
+
+    let mut min_tokens = 0u64;
+    for mut arcade in &lines.flatten().chunks(4) {
+        let a = parse_button(arcade.next().unwrap().as_str());
+        let (ax, ay) = (a.0 as f64, a.1 as f64);
+        let b = parse_button(arcade.next().unwrap().as_str());
+        let (bx, by) = (b.0 as f64, b.1 as f64);
+        let p = parse_prize_p2(arcade.next().unwrap().as_str());
+        let (px, py) = (p.0 as f64, p.1 as f64);
+
+        let ca = (px * by - py * bx) / (ax * by - ay * bx);
+        let cb = (px - ax * ca) / bx;
+        if ca % 1.0 != 0.0 || cb % 1.0 != 0.0 {
+            continue;
+        }
+        min_tokens += ca as u64 * TOKENS_A + cb as u64 * TOKENS_B;
+    }
+    format!("{}", min_tokens)
 }
--- src/solutions/day14.rs
+++ src/solutions/day14.rs
@@ -1,10 +1,127 @@
+use regex::Regex;
+use std::collections::HashMap;
 use std::fs::File;
-use std::io::{BufReader, Lines};
+use std::io::{stdin, BufReader, Lines, Read};
 
-pub fn part1(_lines: Lines<BufReader<File>>) -> String {
-    "Not implemented yet".to_string()
+pub fn part1(lines: Lines<BufReader<File>>) -> String {
+    const WIDTH: i32 = 101;
+    const HEIGHT: i32 = 103;
+    const MID_X: i32 = WIDTH / 2;
+    const MID_Y: i32 = HEIGHT / 2;
+
+    let robot_re = Regex::new("p=(\\d+),(\\d+) v=(-?\\d+),(-?\\d+)").unwrap();
+    let mut quadrants = (0, 0, 0, 0);
+    for line in lines.flatten() {
+        let x = robot_re
+            .captures_iter(line.as_str())
+            .map(|c| c.extract())
+            .map(|(_, [px, py, vx, vy])| {
+                (
+                    px.parse::<i32>().unwrap(),
+                    py.parse::<i32>().unwrap(),
+                    vx.parse::<i32>().unwrap(),
+                    vy.parse::<i32>().unwrap(),
+                )
+            })
+            .next()
+            .unwrap();
+        let mut fx = (x.0 + 100 * x.2) % WIDTH;
+        let mut fy = (x.1 + 100 * x.3) % HEIGHT;
+        if fx < 0 {
+            fx += WIDTH
+        }
+        if fy < 0 {
+            fy += HEIGHT
+        }
+
+        if fx < MID_X && fy < MID_Y {
+            quadrants.0 += 1;
+        } else if fx < MID_X && fy > MID_Y {
+            quadrants.1 += 1;
+        } else if fx > MID_X && fy < MID_Y {
+            quadrants.2 += 1;
+        } else if fx > MID_X && fy > MID_Y {
+            quadrants.3 += 1;
+        }
+    }
+    format!("{}", quadrants.0 * quadrants.1 * quadrants.2 * quadrants.3)
 }
 
-pub fn part2(_lines: Lines<BufReader<File>>) -> String {
-    "Not implemented yet".to_string()
+struct Robot {
+    x: i32,
+    y: i32,
+    vx: i32,
+    vy: i32,
+}
+
+fn display_robots(robots: &Vec<Robot>, width: i32, height: i32) {
+    let mut grid = vec![vec!['.'; width as usize]; height as usize];
+    for r in robots {
+        grid[r.y as usize][r.x as usize] = '#';
+    }
+    for row in grid {
+        for c in row {
+            print!("{}", c);
+        }
+        println!();
+    }
+}
+
+fn maybe_a_tree(robots: &Vec<Robot>, width: i32, height: i32) -> bool {
+    // Attempt 1: Assume the tree is in the middle
+    // let mid_x = width / 2;
+    //
+    // robots.iter().filter(|r| r.x == mid_x).count() as i32 > height / 2
+
+    // Attempt 2: Assume the tree is just somewhere
+    let xs: HashMap<i32, i32> = robots.iter().fold(HashMap::new(), |mut acc, r| {
+        *acc.entry(r.x).or_insert(0) += 1;
+        acc
+    });
+    *xs.values().max().unwrap() > 20
+}
+
+pub fn part2(lines: Lines<BufReader<File>>) -> String {
+    const WIDTH: i32 = 101;
+    const HEIGHT: i32 = 103;
+
+    let robot_re = Regex::new("p=(\\d+),(\\d+) v=(-?\\d+),(-?\\d+)").unwrap();
+    let mut robots: Vec<Robot> = Vec::new();
+    lines.flatten().for_each(|line| {
+        let x = robot_re
+            .captures_iter(line.as_str())
+            .map(|c| c.extract())
+            .map(|(_, [px, py, vx, vy])| Robot {
+                x: px.parse::<i32>().unwrap(),
+                y: py.parse::<i32>().unwrap(),
+                vx: vx.parse::<i32>().unwrap(),
+                vy: vy.parse::<i32>().unwrap(),
+            })
+            .next()
+            .unwrap();
+        robots.push(x);
+    });
+    let mut i = 0;
+    loop {
+        if maybe_a_tree(&robots, WIDTH, HEIGHT) {
+            display_robots(&robots, WIDTH, HEIGHT);
+            println!("Second {}", i);
+            let _ = stdin().read(&mut [0u8]).unwrap();
+        }
+        for r in robots.iter_mut() {
+            r.x = (r.x + r.vx) % WIDTH;
+            r.y = (r.y + r.vy) % HEIGHT;
+            if r.x < 0 {
+                r.x += WIDTH
+            }
+            if r.y < 0 {
+                r.y += HEIGHT
+            }
+        }
+        i += 1;
+        if i > 1000000 {
+            break;
+        }
+    }
+    format!("{}", i)
 }
