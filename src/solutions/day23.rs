40d3699c3368196de05bbd5d24632b1aff92dcf0
Justin Bramley <jbramley@gmail.com>
2024-12-24 06:48:40 -0500

    Day 23 + part 1 of Day 24

--- src/solutions/day23.rs
+++ src/solutions/day23.rs
@@ -1,10 +1,73 @@
+use std::collections::{HashMap, HashSet};
 use std::fs::File;
 use std::io::{BufReader, Lines};
 
-pub fn part1(_lines: Lines<BufReader<File>>) -> String {
-    "Not implemented yet".to_string()
+pub fn part1(lines: Lines<BufReader<File>>) -> String {
+    let mut network: HashMap<String, Vec<String>> = HashMap::new();
+    lines.flatten()
+        .map(|s| {
+            let (s1, s2) = s.split_once("-").unwrap();
+            (s1.to_string(), s2.to_string())
+        })
+        .for_each(|(s1, s2)| {
+            network.entry(s1.clone()).and_modify(|v| v.push(s2.clone())).or_insert(vec![s2.clone()]);
+            network.entry(s2.clone()).and_modify(|v| v.push(s1.clone())).or_insert(vec![s1.clone()]);
+        });
+    let mut targets: HashSet<(&String, &String, &String)> = HashSet::new();
+    for (k, v) in network.iter() {
+        for k2 in v {
+            for k3 in network.get(k2).unwrap() {
+                for k4 in network.get(k3).unwrap() {
+                    if k4 == k && (k.starts_with('t') || k2.starts_with('t') || k3.starts_with('t')) {
+                        let mut v = vec![k, k2, k3];
+                        v.sort();
+                        targets.insert((v[0], v[1], v[2]));
+                    }
+                }
+            }
+        }
+    }
+    println!("{:?}", targets);
+    format!("{}", targets.len())
 }
 
-pub fn part2(_lines: Lines<BufReader<File>>) -> String {
+fn bron_kerbosch<'a>(r: HashSet<&'a str>, p: HashSet<&'a str>, x: HashSet<&'a str>, edges: &HashMap<&str, Vec<&'a str>>) -> Vec<HashSet<&'a str>> {
+    if p.is_empty() && x.is_empty() {
+        return vec![r];
+    }
+    let mut maximal: Vec<HashSet<&str>> = Vec::new();
+    let mut my_p = p.clone();
+    let p_v = Vec::from_iter(p.iter().cloned());
+    let mut my_x = x.clone();
+    for v in p_v {
+        let mut vs: HashSet<&str> = r.clone();
+        vs.insert(v);
+        let n_v: HashSet<&str> = HashSet::from_iter(edges.get(v).unwrap().iter().cloned());
+        maximal.extend(bron_kerbosch(vs, my_p.intersection(&n_v).cloned().collect(), my_x.intersection(&n_v).cloned().collect(), edges));
+        my_p.remove(v);
+        my_x.insert(v);
+    }
+    maximal
+}
+pub fn part2(lines: Lines<BufReader<File>>) -> String {
+    let mut network: HashMap<&str, Vec<&str>> = HashMap::new();
+    let lines = lines.flatten().collect::<Vec<String>>();
+    for s in lines.iter() {
+        let (s1, s2) = s.split_once("-").unwrap();
+        network.entry(s1).and_modify(|v| v.push(s2)).or_insert(vec![s2]);
+        network.entry(s2).and_modify(|v| v.push(s1)).or_insert(vec![s1]);
+    }
+
+    let nodes = network.iter().map(|(k,_)| *k).collect::<HashSet<_>>();
+    let maximals = bron_kerbosch(HashSet::new(), nodes, HashSet::new(), &network);
+    let mut supermax: HashSet<&str> = HashSet::new();
+    for m in maximals {
+        if m.len() > supermax.len() {
+            supermax = m;
+        }
+    }
+    let mut supermax = Vec::from_iter(supermax.iter().cloned());
+    supermax.sort();
+    println!("{}", supermax.join(","));
     "Not implemented yet".to_string()
 }
--- src/solutions/day24.rs
+++ src/solutions/day24.rs
@@ -1,8 +1,105 @@
+use std::collections::HashMap;
 use std::fs::File;
 use std::io::{BufReader, Lines};
+use itertools::Itertools;
+use topological_sort::TopologicalSort;
 
-pub fn part1(_lines: Lines<BufReader<File>>) -> String {
-    "Not implemented yet".to_string()
+
+#[derive(Debug)]
+enum Operation {
+    AND,
+    OR,
+    XOR
+}
+
+impl Operation {
+    fn exec(&self, a: bool, b: bool) -> bool {
+        match self {
+            Operation::AND => a & b,
+            Operation::OR => a | b,
+            Operation::XOR => a ^ b,
+        }
+    }
+}
+
+impl From<&str> for Operation {
+    fn from(value: &str) -> Self {
+        match value {
+            "AND" => Operation::AND,
+            "OR" => Operation::OR,
+            "XOR" => Operation::XOR,
+            _ => panic!()
+        }
+    }
+}
+
+#[derive(Debug)]
+struct Gate {
+    inputs: [String; 2],
+    op: Operation,
+    output: Vec<String>,
+}
+
+
+pub fn part1(lines: Lines<BufReader<File>>) -> String {
+    let mut wires: HashMap<String, bool> = HashMap::new();
+    let mut gates: HashMap<String, Gate> = HashMap::new();
+    let mut rmap_gates: HashMap<String, String> = HashMap::new();
+
+    for line in lines.flatten() {
+        if line.contains(":") {
+            let w = line[..3].to_string();
+            let b = line.ends_with("1");
+            // println!("Init: {} -> {}", w.clone(), b);
+            wires.insert(w, b);
+        } else if line.contains("->") {
+            let v = line.split_ascii_whitespace().collect::<Vec<_>>();
+            let n = format!("{} {} {}", v[0], v[1], v[2]);
+            gates.entry(n.clone())
+                .and_modify(|g| g.output.push(v[4].to_string()))
+                .or_insert(Gate {
+                    inputs: [v[0].to_string(), v[2].to_string()],
+                    op: Operation::from(v[1]),
+                    output: vec![v[4].to_string()]
+                });
+            wires.insert(v[4].to_string(), false);
+            rmap_gates.insert(v[4].to_string(), n.clone());
+        }
+    }
+
+    let mut ts = TopologicalSort::<&str>::new();
+    for (n, g) in gates.iter() {
+        if let Some(dep) = rmap_gates.get(&g.inputs[0]) {
+            ts.add_dependency(dep.as_str(), n.as_str());
+        }
+        if let Some(dep) = rmap_gates.get(&g.inputs[1]) {
+            ts.add_dependency(dep.as_str(), n.as_str());
+        }
+    }
+
+    while !ts.is_empty() {
+        let n = ts.pop().unwrap();
+        let g = gates.get(n).unwrap();
+        let a = wires.get(&g.inputs[0]).unwrap().clone();
+        let b = wires.get(&g.inputs[1]).unwrap().clone();
+        for out in g.output.iter() {
+            wires.entry(out.clone()).and_modify(|d| *d = g.op.exec(a,b));
+        }
+        // println!("{} -> {:?}: {} {:?} {} -> {}", n, g.output, a, g.op, b, g.op.exec(a,b));
+    }
+
+    let mut ans = 0u64;
+    for (i, z) in wires.keys()
+        .filter(|k| k.starts_with("z"))
+        .sorted()
+        .enumerate() {
+        if wires[z] {
+            ans += 2u64<<(i-1);
+        }
+        // print!("{}", if wires[z] { "1" } else { "0" });
+        
+    }
+    format!("{}", ans)
 }
 
 pub fn part2(_lines: Lines<BufReader<File>>) -> String {
